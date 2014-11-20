c114119680ab3d4c8be46e272614417b4474a2dc
Salvatore Santagati <Salvatore.Santagati@gmail.com>
2014-11-20 14:23:07 +0100

    Added act2tcx lib
    Convert Act  ( Globalsat ) format to TCX ( Garmin ) format

---
+++ act2tcx.php
@@ -0,0 +1,293 @@
+<?php
+
+/* Copyright (C) 2014 Salvatore Santagati <salvatore.santagati@gmail.com> 
+ */
+
+class act2tcx {
+
+	private $Sport_t;
+	private $Id;
+	private $dateTime;
+	private $ttseconds;
+	private $distancemeters;
+	private $AvgHeartRate;
+	private $MaxHearRate;
+	private $Cadence;
+	private $Tracks;
+	private $TimeTrack;
+	private $track;
+	private $LatitudeDegrees;
+	private $LongitudeDegrees;
+	private $AltitudeMeters;
+	private $HeartRateBpm;
+	private	$CadenceTrack;
+
+
+
+
+	function __construct ( $act ) {
+			
+
+		$this->setActivitySport ( $act );
+		$this->setId ( $act );
+		$this->setStarttime ( $act );
+		$this->setTotalTimeSeconds ( $act );
+		$this->setDistanceMeters ( $act );
+		$this->setCalories ( $act );
+		$this->setAverageHeartRateBpm ($act);
+		$this->setMaxHearRate ($act);
+		$this->setAvgCadence ($act);
+		$this->setTracks ($act);
+		$this->setTrackPoints ( $act );
+	
+	}
+
+	function activities ()	{
+
+		
+	}
+
+	function hoursToSeconds ($hour) { // $hour must be a string type: "HH:mm:ss"
+
+		$parse = array();
+
+    		if (!preg_match ('#^(?<hours>[\d]{2}):(?<mins>[\d]{2}).(?<secs>[\d]{2})$#',$hour,$parse)) {
+         	// Throw error, exception, etc
+        		throw new RuntimeException ("Hour Format not valid");
+    		}
+
+		return $parse['hours'] * 3600 + $parse['mins'] * 60 + $parse['secs'];
+	}
+
+	function setActivitySport ( $act ) 	{
+
+		
+		switch ($act->trackmaster->Sport1) {
+
+		case	0:
+		case	1:
+		case	2: 
+				$this->Sport_t = "Running";
+				break;
+		case	3:	$this->Sport_t = "Biking";
+				break;
+		case	4:
+		case	5:	$this->Sport_t = "Other";
+				break;
+		default :
+				$this->Sport_t = "";
+	
+		}
+
+	}
+
+	function setId ( $act )		{
+
+		$this->Id = $act->trackmaster->TrackName . "T" . $act->trackmaster->StartTime . "Z";
+		
+	}
+
+	function setStarttime ( $act )	{
+
+		$this->dateTime =  $act->trackmaster->TrackName . "T" . $act->trackmaster->StartTime . "Z";
+	}
+
+
+
+	function setTotalTimeSeconds ( $act ) {
+	
+		$this->ttseconds =  ( $this->hoursToSeconds ( $act->trackmaster->Duration ) );
+
+	}
+
+	function setDistanceMeters ( $act )	{
+
+		$this->Distancemeters = $act->trackmaster->TotalDist;
+	
+	
+	}
+
+	function setCalories ( $act ) {
+
+		$this->Calories = $act->trackmaster->Calories;
+	}	
+
+
+	function setAverageHeartRateBpm ( $act ) {
+		
+		$this->AvgHeartRate = $act->trackmaster->AvgHeartRate;
+	}
+
+	function setMaxHearRate ($act) {
+
+		$this->MaxHearRate = $act->trackmaster->MaxHearRate;
+	
+	}
+
+	function setAvgCadence ( $act ) {
+
+		$this->Cadence	=  $act->trackmaster->AvgCadence;
+	}
+
+	function setTracks ( $act ){
+
+		$this->Tracks = count ($act->TrackPoints );
+	}
+
+	function setTimeTrack ( $act, $track , $value ){
+ 
+		$this->TimeTrack[$track] = $value;
+	
+	}
+
+	
+
+	function setTrackPoints( $act ){
+		
+	       $this->CurrentTime = new DateTime ($this->getStarttime()) ;		
+
+	       for ( $this->track = 0; $this->track < $this->getTracks (); $this->track++) {
+			
+		       /* TIME */
+		       $this->TimeTrack[$this->track] = $this->CurrentTime->format('Y-m-d\TH:i:s\Z');
+		       /* Fix Format of intervaltime */
+		       $this->IntervalTime =  round ( str_replace(".", ";",  $act->TrackPoints[$this->track]->IntervalTime ) );
+		       $this->CurrentTime->add(new DateInterval('PT' . $this->IntervalTime . 'S'));
+
+		       /* Latitude */
+		       $this->LatitudeDegrees[$this->track] = ( str_replace(",", "." , $act->TrackPoints[$this->track]->Latitude ) );
+
+		       /* Longitude */
+		       $this->LongitudeDegrees[$this->track] = ( str_replace(",", "." , $act->TrackPoints[$this->track]->Longitude ) );
+
+		       /* Altitude */
+		       $this->AltitudeMeters[$this->track] = $act->TrackPoints[$this->track]->Altitude;
+
+		       /* HeartRate */
+		       $this->HeartRateBpm[$this->track] = $act->TrackPoints[$this->track]->HeartRate;
+
+		       /* Cadence */
+		       $this->CadenceTrack[$this->track] = $act->TrackPoints[$this->track]->Cadence;
+			
+
+	       }
+	}
+
+	function setLatitude( $act, $track , $value) {
+		$this->LatitudeDegrees[$track] = $value;
+	}
+	
+	function setLongitude( $act, $track , $value){
+		$this->LongitudeDegrees[$track] = $value;
+	}
+
+	function setAltitude($act, $track , $value){
+		
+		$this->AltitudeMeters[$track] = $value;
+	
+	}
+
+	function setHeartRate($act, $track , $value){
+
+		$this->HeartRate[$track] = $value;
+	}
+
+	function setCadenceTrack ($act, $track, $value) {
+		
+		$this->CadenceTrack[$track] = $value;
+	}
+	
+	
+	function getCadenceTrack ($track) {
+
+		return $this->CadenceTrack[$track];
+
+	}
+
+
+	function getHeartRate ($track){
+		return $this->AltitudeMeters[$track];
+	}
+
+	function getAltitude($track){
+		return $this->AltitudeMeters[$track];
+	}
+	
+	function getLongitude($track){
+		return $this->LongitudeDegrees[$track];
+	}
+
+
+	
+	function getLatitude($track) {
+
+		return $this->LatitudeDegrees[$track];
+			
+	}
+
+	function getTimeTrack ( $track ){
+
+		return $this->TimeTrack[$track];
+	}
+
+	function getTracks () {
+		
+		return $this->Tracks;
+
+	}
+
+	function getAvgCadence () {
+		
+		return $this->Cadence;
+
+	}
+
+	function getMaxHearRate () {
+
+		return $this->MaxHearRate;
+	
+	}
+
+	function getAverageHeartRateBpm ( )  {
+
+		return $this->AvgHeartRate;
+	}
+
+	function getCalories (  ) {
+
+		return $this->Calories;
+	}
+	function getDistanceMeters (  ) {
+
+		return $this->Distancemeters;
+	
+	}
+
+	function getTotalTimeSeconds () {
+
+		return $this->ttseconds;
+
+	}
+
+
+	function getStarttime()		{
+
+		return $this->dateTime;
+	}	
+
+
+	
+	function getId ()		{
+
+		return $this->Id;
+	}
+
+	function getActivitySport()	{
+
+		return $this->Sport_t;
+	
+	}
+
+}
+
+?>
